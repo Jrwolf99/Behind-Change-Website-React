2e6c4e0f66725cff742a8a70859c2aab35e732a1
jrwolf99 <jrwolf99@outlook.com>
2022-03-22 13:26:18 -0500

    fixed up the bubbles

--- src/pages/home/mobile_sections/HeroSection/HeroSection.js
+++ src/pages/home/mobile_sections/HeroSection/HeroSection.js
@@ -8,11 +8,35 @@ import "../../../../globalstyles/utility.css";
 import MNavBar from "../../../../components/mobile/navbar/NavBar";
 import MHeroText from "../../../../components/mobile/hero-text/HeroText";
 
+//assets
+import Bubbles1 from "../../../../assets/images/mobileheaderbubbles1.svg";
+import Bubbles2 from "../../../../assets/images/mobileheaderbubbles2.svg";
+import Bubbles3 from "../../../../assets/images/mobileheaderbubbles3.svg";
+import Bubbles4 from "../../../../assets/images/mobileheaderbubbles4.svg";
+
 export default function HeroSection() {
   return (
     <div className={styles["hero-section"]}>
       <MNavBar />
       <MHeroText />
+      <div className={styles["bubbles"]}>
+        <img
+          src={Bubbles1}
+          className={`${styles["bubble"]} ${styles["bubble1"]}`}
+        />
+        <img
+          src={Bubbles2}
+          className={`${styles["bubble"]} ${styles["bubble2"]}`}
+        />
+        <img
+          src={Bubbles3}
+          className={`${styles["bubble"]} ${styles["bubble3"]}`}
+        />
+        <img
+          src={Bubbles4}
+          className={`${styles["bubble"]} ${styles["bubble4"]}`}
+        />
+      </div>
     </div>
   );
 }
