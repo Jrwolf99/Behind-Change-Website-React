f69382c6a0fb802e979cdff516bbd512f84cfede
jrwolf99 <jrwolf99@outlook.com>
2022-03-23 23:13:06 -0500

    added page move

--- src/components/desktop/action-text/ActionText.js
+++ src/components/desktop/action-text/ActionText.js
@@ -11,7 +11,7 @@ export default function ActionText() {
         Make an <strong>impact!</strong>
         <br /> Let’s get in touch.
       </p>
-      <Link className={styles["mylink"]}>
+      <Link className={styles["mylink"]} to="/contact">
         <p className={styles["mylink-text"]}>Start a proposal &gt;</p>
       </Link>
     </div>
