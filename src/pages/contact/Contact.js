be307d5c612f39ff745ab4daae19d0de95150afe
jrwolf99 <jrwolf99@outlook.com>
2022-03-28 21:26:09 -0500

    cleaned up portfolio page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -6,15 +6,22 @@ import "../../globalstyles/typography.css";
 import ContactForm from "../../components/desktop/contact-form/ContactForm";
 import NavBar from "../../components/desktop/navbar/NavBar";
 
+//assets
+import Bubbles1 from "../../assets/images/DBubble1.svg";
+import Bubbles2 from "../../assets/images/DBubble2.svg";
+import Bubbles3 from "../../assets/images/DBubble3.svg";
+import Bubbles4 from "../../assets/images/DBubble4.svg";
+import Rocket from "../../assets/images/rocket.png";
+
 export default function Contact() {
   return (
     <div className={styles["contact-page-gradient"]}>
-      <div className={styles["rocket-logo"]}>
-        <div className={styles["nav"]}>
-          <NavBar buttonclr="blue" />
-        </div>
+      <div className={styles["nav"]}>
+        <NavBar buttonclr="blue" />
+      </div>
 
-        <div className={styles["rocket"]}></div>
+      <div className={styles["rocket"]}>
+        <img src={Rocket} alt="a rocket" />
       </div>
 
       <div className={styles["contact-page"]}>
@@ -23,8 +30,31 @@ export default function Contact() {
           <br />
           <strong>Chat.</strong>
         </p>
-
         <ContactForm></ContactForm>
+
+        <div className={styles["bubbles"]}>
+          <img
+            src={Bubbles1}
+            className={`${styles["bubble"]} ${styles["bubble1"]}`}
+            alt="bubbles on header for mobile"
+          />
+          <img
+            src={Bubbles2}
+            className={`${styles["bubble"]} ${styles["bubble2"]}`}
+            alt="bubbles on header for mobile"
+          />
+          <img
+            src={Bubbles3}
+            className={`${styles["bubble"]} ${styles["bubble3"]}`}
+            alt="bubbles on header for mobile"
+          />
+          <div></div>
+          <img
+            src={Bubbles4}
+            className={`${styles["bubble"]} ${styles["bubble4"]}`}
+            alt="bubbles on header for mobile"
+          />
+        </div>
       </div>
     </div>
   );
