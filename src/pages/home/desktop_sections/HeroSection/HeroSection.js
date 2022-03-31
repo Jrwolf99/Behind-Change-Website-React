65f02ed18b0cfd8ced6a8b61e824ec3bb3d15030
Jonathan <jrwolf99@outlook.com>
2022-03-30 20:26:37 -0500

    made changes to contact page

--- src/pages/home/desktop_sections/HeroSection/HeroSection.js
+++ src/pages/home/desktop_sections/HeroSection/HeroSection.js
@@ -8,17 +8,14 @@ import "../../../../globalstyles/utility.css";
 import NavBar from "../../../../components/desktop/navbar/NavBar";
 import HeroText from "../../../../components/desktop/hero-text/HeroText";
 import HeroWindow from "../../../../components/desktop/hero-window/HeroWindow";
-import MyCanvas from "../../../../components/tools/canvas/Canvas";
 
 export default function HeroSection() {
-  const drawDashLine = (c, frameCount) => {};
-
   return (
     <div className={styles["hero-section"]}>
       <NavBar />
       <div
         className="flex-wrapper-row"
-        style={{ justifyContent: "space-between", alignItems: "flex-start" }}
+        style={{ justifyContent: "space-around", alignItems: "flex-start" }}
       >
         <HeroText />
         <HeroWindow />
