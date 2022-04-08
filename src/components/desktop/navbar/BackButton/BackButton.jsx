da4e28e7c4d4c39fb44a90560f5541162c0b1bca
jrwolf99 <jrwolf99@outlook.com>
2022-04-08 15:56:44 -0500

    added button animation

---
+++ src/components/desktop/navbar/BackButton/BackButton.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import styles from "./BackButton.module.css";
+
+import { motion, useAnimation } from "framer-motion";
+
+import { useToggle } from '../../../../hooks/useToggle';
+
+
+export default function BackButton() {
+    const [isHovered, toggleHover, setIsHovered] = useToggle();
+    return (
+        <div
+            onMouseOver={() => { setIsHovered(true) }}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <motion.div
+                animate={isHovered ? { rotate: -45 } : {}}
+                className={`${styles['bar']}`} />
+            <motion.div
+                animate={isHovered ? { rotate: 225, x: 0, y: -13 } : {}}
+                className={`${styles['bar']}`} />
+            <motion.p
+                animate={isHovered ? { scale: 1.1 } : {}}
+                className={`${styles['text']}`}>Back To<br />Home</motion.p>
+        </div>
+    )
+}
--- src/components/desktop/navbar/NavBar.js
+++ src/components/desktop/navbar/NavBar.js
@@ -5,10 +5,9 @@ import styles from "./NavBar.module.css";
 //assets
 import ProposalButton from "../../../assets/images/ProposalButton.svg";
 import { Link } from "react-router-dom";
+import BackButton from "./BackButton/BackButton";
 
 export default function Nav({ buttontype, buttonpressed }) {
-  var LinkClassName = "";
-
   return (
     <nav className={styles["myNav"]}>
       <Link className={styles["logo"]} to="/">
@@ -16,19 +15,8 @@ export default function Nav({ buttontype, buttonpressed }) {
         <p className={`${"playfair-600"} ${styles["change"]}`}>&nbsp;Change</p>
       </Link>
       {buttontype === "proposal" ? (
-        <Link
-          onMouseOver={() => {
-            LinkClassName = "open";
-            console.log(LinkClassName);
-          }}
-          className={"open"}
-          to="/"
-        >
-          <div className={`${styles["bar"]} ${styles["bar1"]}`}></div>
-          <div className={`${styles["bar"]} ${styles["bar2"]}`}></div>
-          <p>
-            Back to <br /> Home
-          </p>
+        <Link to="/">
+          <BackButton />
         </Link>
       ) : (
         <Link to="/contact">
