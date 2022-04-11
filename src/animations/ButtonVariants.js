1c2188f9d74d5cfd339e34545e386b05179f192b
jrwolf99 <jrwolf99@outlook.com>
2022-04-10 21:24:13 -0500

    added answers to desktop

---
+++ src/animations/ButtonVariants.js
@@ -0,0 +1,13 @@
+export const ButtonVariants = {
+  initial: {
+    x: "100vw",
+  },
+  visible: {
+    x: 0,
+    transition: { ease: "easeInOut", duration: 1.2 },
+  },
+  exit: {
+    x: "100vw",
+    transition: { ease: "easeInOut", duration: 1.2 },
+  },
+};
--- src/components/desktop/question-grid/inner-grid/InnerGrid.jsx
+++ src/components/desktop/question-grid/inner-grid/InnerGrid.jsx
@@ -1,19 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './InnerGrid.module.css'
 import "../../../../globalstyles/typography.css";
 
+import { motion } from 'framer-motion';
 
 export default function InnerGrid() {
+
+
+    const [buttonFlip1, setButtonFlip1] = useState(false);
+    const [buttonFlip2, setButtonFlip2] = useState(false);
+    const [buttonFlip3, setButtonFlip3] = useState(false);
+    const [buttonFlip4, setButtonFlip4] = useState(false);
+
     return (
         <div className={styles['inner-grid']}>
-            <div className={styles["q1"]}>+ How long does the process take?</div>
-            <div className={styles["q2"]}>+ Is your service Really free?</div>
-            <div className={styles["q3"]}>
-                + Who owns the rights to the website once it’s finished?
-            </div>
-            <div className={styles["q4"]}>
-                + What happens if I dislike the design?
-            </div>
+            <motion.button className={styles["q1"]} onClick={() => setButtonFlip1(!buttonFlip1)}>
+                {buttonFlip1 ? (<p>
+                    + How long does the process take?</p>)
+                    : (<p>Anywhere from 2 weeks to 3 months,
+                        respectively. Every project is different,
+                        with varying levels of complexity and
+                        size.</p>)}
+            </motion.button>
+            <motion.button className={styles["q2"]} onClick={() => setButtonFlip2(!buttonFlip2)}>
+                {buttonFlip2 ? (<p>
+                    + Is your service Really free?</p>)
+                    : (<p>Yes! There are no hidden fees for
+                        contacting or utilizing our services.
+                        We want to build a portfolio
+                        that helps others and (hopefully)
+                        creates positive change!</p>)}
+            </motion.button>
+            <motion.button className={styles["q3"]} onClick={() => setButtonFlip3(!buttonFlip3)}>
+                {buttonFlip3 ? (<p>
+                    + Who owns the rights to the website once it’s finished?</p>)
+                    : (<p>
+                        You! While we will ask for the
+                        right to share your website within
+                        our portfolio, you will have complete
+                        control over every aspect of your
+                        site once we’re finished.
+                    </p>)}
+            </motion.button>
+            <motion.button className={styles["q4"]} onClick={() => setButtonFlip4(!buttonFlip4)}>
+                {buttonFlip4 ? (<p>
+                    + What happens if I dislike the design?</p>)
+                    : (<p>
+                        We’ll change it!
+                        We don’t begin developing
+                        your website until after you’ve
+                        approved the design. It’s likely
+                        that your site will go through
+                        5 to 20 iterations before a design
+                        is finalized. Don’t hesitate
+                        to critique! This is your website!
+                    </p>)}
+            </motion.button>
         </div>)
 }
